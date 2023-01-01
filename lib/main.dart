e696af17e0cb1609b52ac8002ef1ebb247f8d653
user <savaliyaparth712@gmail.com>
2023-01-01 22:02:59 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'color.dart';
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
